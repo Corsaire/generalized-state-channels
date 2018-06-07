558c7afda43b71ea033f0be34ccc2bd6aaf11380
mestrecorsaire <mestrecorsaire@gmail.com>
2018-06-07 19:07:13 +0300

    update

--- contracts/IStateMachine.sol
+++ contracts/IStateMachine.sol
@@ -3,5 +3,5 @@ pragma solidity 0.4.23;
 contract IStateMachine {
 
     function verifyTx(bytes state, bytes transaction, bytes newState) public returns(bool);
-    function tryClose(bytes state) public returns(bool);
+    function tryClose() public returns(uint[2] refund);
 }
--- contracts/StateChannelsManager.sol
+++ contracts/StateChannelsManager.sol
@@ -4,14 +4,15 @@ import "./IStateMachine.sol";
 
 contract StateChannelsManager {
 
-    enum Status { Initialize, Started, Dispute }
-
+    enum Status { Initialize, Started, Refunding, Closed }
+    
     struct Player
     {
         bool registred;
         address addr;
         uint deposit;
-        uint extra;        
+        uint extra;  
+        uint refund;      
     }
 
     struct State
@@ -24,14 +25,10 @@ contract StateChannelsManager {
     State lastState;
     IStateMachine sm;
 
-    uint initialized = 0; 
-
     uint deadline;
     
-
     mapping(address => Player) players;
-    address player1;
-    address player2;
+    address[2] playersAds;
 
     modifier isPlayer()
     {
@@ -45,15 +42,30 @@ contract StateChannelsManager {
         _;
     }
 
+    modifier isStatuses(Status[] _statuses)
+    {
+        bool t = false;
+        for(uint i = 0; i < _statuses.length; i++)
+            if(_statuses[i] == status)
+            {
+                t = true;
+                break;
+            }
+        require(t);
+        _;
+    }
+
     constructor(address[] _ads, uint[] _deposits, uint[] _extras) public 
     {
-        playersCount = _ads.length;
-        require(playersCount == 2);
-        require(playersCount == _deposits.length);
-        require(playersCount == _extras.length);
+        require(2 == _ads.length);
+        require(2 == _deposits.length);
+        require(2 == _extras.length);
 
         for(uint i = 0; i < _ads.length; i++)
             players[_ads[i]] = Player(false, _ads[i], _deposits[i], _extras[i]);
+
+        playersAds[0] = _ads[0];
+        playersAds[1] = _ads[1];
     }
 
     function register() public payable
@@ -64,49 +76,48 @@ contract StateChannelsManager {
         require(player.registred == false);
         require(player.deposit + player.extra == msg.value);
         player.registred = true;
-        initialized++;
-        if(playersCount == initialized)        
+        
+        if(players[playersAds[0]].registred && players[playersAds[1]].registred)        
             start();
     }
 
     function start() private 
     {
         status = Status.Started;
-
     }
 
-    function close() public
+    function closeChannel() public
+    isPlayer
     {
-        if(sm.canClose())
-        {
-            
-        }
+        require(status == Status.Started || status == Status.Initialize);
+        uint[2] memory refunds = sm.tryClose();
+        for(uint i = 0; i < 2; i++)
+            players[playersAds[i]].refund = refunds[i];
+        status = Status.Refunding;
     }
 
-    function disputeWaitingForTX() public
+    function withdraw() public
+    isPlayer
+    isStatus(Status.Refunding)
     {
-
-    }
-
-    function disputeWaitingForSig() public
-    {
-
     }
 
     /**
         This method is called by a party, if counterparty denies to sign transaction.
      */
-    function subitTX(bytes transaction, bytes newState) 
+    function subitTX(uint txNumber, bytes transaction, bytes newState) 
     isPlayer
     public
     {
         sm.verifyTx(lastState.state, transaction, newState);
+        lastState.state = State(newState, txNumber);
     }
 
-    function submitState(uint txCount, bytes state, uint8 v1, bytes sig1, bytes sig2) external
+    function submitState(uint txNumber, bytes state, uint8 v1, bytes sig1, bytes sig2) 
+    external
     {
-        require(txCount > lastState.txNumber);
-        bytes32 stateHash = keccak256(txCount, state, address(this));
+        require(txNumber > lastState.txNumber);
+        bytes32 stateHash = keccak256(txNumber, state, address(this));
         require(confirmState(player1, stateHash, sig1));
         require(confirmState(player2, stateHash, sig2));
     }
